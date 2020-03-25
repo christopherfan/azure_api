44270a81886e76479c6857a6662a4c5146cdf53b
christopherfan <christopher.c.fan@gmail.com>
2020-03-24 23:16:25 -0700

    Zip Changes

--- LocalFunctionProj/HttpExample/__init__.py
+++ LocalFunctionProj/HttpExample/__init__.py
@@ -7,6 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    zip = req.params.get('zip')
     if not name:
         try:
             req_body = req.get_json()
@@ -17,6 +18,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if name:
         return func.HttpResponse(f"Hello {name}!")
+    if zip:
+        if zip =='94530':
+            return func.HttpResponse("400")
+        else: 
+            return func.HttpResponse(f"Hello {zip}!")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
